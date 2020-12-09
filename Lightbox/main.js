55899ca297d66f9e06094d07584672fec8372eb1
szymonrajskiwsei <72198126+szymonrajskiwsei@users.noreply.github.com>
2020-12-09 17:39:03 +0100

    lightbox update

--- Lightbox/main.js
+++ Lightbox/main.js
@@ -4,22 +4,41 @@ lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 const btnPrev = document.getElementById('btnPrev');
 const btnNext = document.getElementById('btnNext');
+let currentImg;
+const img = document.createElement('img');
 
-const images = document.querySelectorAll('img');
+const images = [...document.querySelectorAll('img')]; // na tablice
 images.forEach(image => {
     image.addEventListener('click', f => {
 
+        currentImg = image.getAttribute("data-number");
         lightbox.classList.add('active');
-        const img = document.createElement('img');
+
         img.src = image.src;
         while(lightbox.firstChild)
         {
             lightbox.removeChild(lightbox.firstChild);
         }
+
         lightbox.appendChild(img);
         menu.classList.add('menu__active');
         lightbox.appendChild(menu);
+        console.log(currentImg);
     });
+});
+
+console.log(images);
+
+btnPrev.addEventListener('click', () => {
+    currentImg--;
+    if(currentImg < 0) currentImg = 5;
+    img.src = images[currentImg].src;
+});
+
+btnNext.addEventListener('click', () => {
+    currentImg++;
+    if(currentImg > 5) currentImg = 0;
+    img.src = images[currentImg].src;
 });
 
 lightbox.addEventListener('click', f => {
