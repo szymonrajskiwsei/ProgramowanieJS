42819c0ed72bcd19d85ac911ae51eb167cfe617c
szymonrajskiwsei <72198126+szymonrajskiwsei@users.noreply.github.com>
2020-12-20 15:49:36 +0100

    .

--- Weather/main.js
+++ Weather/main.js
@@ -1,17 +1,16 @@
+// TO DO Wysłać do url nazwę miasta w zapytaniu
+
+const btnAddCity = document.getElementById("btnAddCity");
+const citiesDiv = document.getElementById('citiesDiv');
+
 let cityName = "";
 let opwApiKey = "50d53005c0fd5f556bb4ef15224c4209";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=Tarnów&APPID=${opwApiKey}`;
-
-function AddCity()
-{
-    cityName = document.getElementById("inputCity").value;
-    
-    alert(cityName);
-    alert(url);   
-}
+let url = `http://api.openweathermap.org/data/2.5/weather?q=Warszawa&APPID=${opwApiKey}`;
+let citiesArray = [];
+let ls = window.localStorage.getItem("cities");
+if (ls == null) ls = window.localStorage.setItem("cities", citiesArray);
 
 let http = new XMLHttpRequest();
-
 http.addEventListener("readystatechange", httpStateChange);
 //http.addEventListener("progress", httpProgressChange);
 //http.addEventListener("error", httpError);
@@ -19,13 +18,36 @@ http.addEventListener("readystatechange", httpStateChange);
 http.open("GET", url, true);
 http.send();
 
+function AddCity()
+{
+    cityName = document.getElementById("inputCity").value;
+    
+    citiesArray.push(cityName);
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${opwApiKey}`;  
+}
+
+function CreateCityDiv(cityInfo)
+{
+    let city = document.createElement("div");
+    let cityName = document.createElement("p");
+
+    city.classList.add("city");
+    cityName.classList.add("cityName");
+
+    cityName.innerHTML = cityInfo;
+
+    city.appendChild(cityName);
+    citiesDiv.appendChild(city);
+}
+
 function httpStateChange(e)
 {
     console.log(`Http state change: ${e.target.readyState}`);
     console.log(`Http status code: ${e.target.status}`);
 
-    if(e.target.readyState == "4")
+    if (e.target.readyState == "4")
     {
         console.log(e.target.responseText);
+        btnAddCity.addEventListener("click", () => CreateCityDiv(e.target.responseText));
     }
 }
