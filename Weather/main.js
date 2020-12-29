8f2c14949909f30445f25c212220db33350a9d87
szymonrajskiwsei <72198126+szymonrajskiwsei@users.noreply.github.com>
2020-12-29 19:53:04 +0100

    Update main.js

--- Weather/main.js
+++ Weather/main.js
@@ -1,53 +1,27 @@
-// TO DO Wysłać do url nazwę miasta w zapytaniu
-
-const btnAddCity = document.getElementById("btnAddCity");
-const citiesDiv = document.getElementById('citiesDiv');
-
-let cityName = "";
-let opwApiKey = "50d53005c0fd5f556bb4ef15224c4209";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=Warszawa&APPID=${opwApiKey}`;
-let citiesArray = [];
-let ls = window.localStorage.getItem("cities");
-if (ls == null) ls = window.localStorage.setItem("cities", citiesArray);
-
-let http = new XMLHttpRequest();
-http.addEventListener("readystatechange", httpStateChange);
-//http.addEventListener("progress", httpProgressChange);
-//http.addEventListener("error", httpError);
-
-http.open("GET", url, true);
-http.send();
-
-function AddCity()
+class Weather
 {
-    cityName = document.getElementById("inputCity").value;
-    
-    citiesArray.push(cityName);
-    url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${opwApiKey}`;  
-}
-
-function CreateCityDiv(cityInfo)
-{
-    let city = document.createElement("div");
-    let cityName = document.createElement("p");
-
-    city.classList.add("city");
-    cityName.classList.add("cityName");
-
-    cityName.innerHTML = cityInfo;
-
-    city.appendChild(cityName);
-    citiesDiv.appendChild(city);
-}
-
-function httpStateChange(e)
-{
-    console.log(`Http state change: ${e.target.readyState}`);
-    console.log(`Http status code: ${e.target.status}`);
-
-    if (e.target.readyState == "4")
+    constructor(config)
     {
-        console.log(e.target.responseText);
-        btnAddCity.addEventListener("click", () => CreateCityDiv(e.target.responseText));
+        const {wrapper, form} = config;
+        this.wrapper = wrapper;
+        this.form = form;
+
+        this.data = [];
+        this.API = `http://api.openweathermap.org/data/2.5/weather?APPID=50d53005c0fd5f556bb4ef15224c4209&lang=pl&units=metric`
+
+        if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll))
+        {
+            this.init()
+        }
+        else
+        {
+            document.addEventListener("DOMContentLoaded", () => this.init());
+        }
+    }
+
+    init()
+    {
+
+        
     }
 }
